3dda509998dc5dbc2744f6148cf9109057eda795
isaacjameses <isaacjames@alu.ufc.br>
2019-04-11 01:53:37 -0300

    mondal on card

--- src/component/card.js
+++ src/component/card.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Card from 'react-bootstrap/Card'
-import {Button} from 'react-bootstrap'
+import {Button, ModalBody} from 'react-bootstrap'
 import {connect} from 'react-redux'
 import Modal from 'react-bootstrap/Modal'
 // material-ui
@@ -19,9 +19,13 @@ import PrateleiraApi from '../service/PrateleiraAPI'
     super(props);  
     this.state = {
       show: false,
+      ModalCard: false
     };
     this.handleShow = this.handleShow.bind(this);
     this.handleClose = this.handleClose.bind(this);
+    this.handleCloseModalCard = this.handleCloseModalCard.bind(this);
+    this.handleShowModalCard = this.handleShowModalCard.bind(this)
+    this.MyModal = this.MyModal.bind(this);
   }
 
   handleClose() {
@@ -31,39 +35,76 @@ import PrateleiraApi from '../service/PrateleiraAPI'
   handleShow() {
     this.setState({ show: true });
   }
+
+  handleShowModalCard() {
+    this.setState({ ModalCard: true });
+  }
+
+  handleCloseModalCard() {
+    this.setState({ ModalCard: false });
+  }
+
+  MyModal = ()=>
+    <Modal style={{height: '60vw'}} show={this.state.ModalCard} onHide={()=> this.setState({ModalCard: false})}>
+      <ModalBody >
+          <Card>
+            <Card.Img variant="top" src={this.props.tratarPath(this.props.produto.thumbnail)}/>
+            <Card.Body>
+              <Card.Title className="title" >
+                {this.props.produto.title}
+              </Card.Title>
+              <Card.Text>
+              {this.props.produto.description}
+              </Card.Text>
+              <IconButton className="icon" onClick={() => {this.handleShow(); this.props.moveTo(this.props.produto, 'PROMOCOES')}}>
+                <FavoriteIcon />
+              </IconButton>
+              <IconButton className="icon" onClick={() => {this.handleShow(); this.props.moveTo(this.props.produto, 'FAVORITOS')}}>
+                <BookMarkIcon /> 
+              </IconButton>
+              <IconButton className="icon" onClick={() => {this.handleShow(); this.props.moveTo(this.props.produto, 'COMPRADOS')}}>
+                <MoneyOff />
+              </IconButton>
+            </Card.Body>
+          </Card>
+        </ModalBody>
+    </Modal>
+
   render(){
     return (
       <div>
         <Card className="card">
-        <Card.Img variant="top" src={this.props.tratarPath(this.props.produto.thumbnail)}/>
-        <Card.Body>
-          <Card.Title className="title" >{this.props.produto.title}</Card.Title>
-          <IconButton className="icon" onClick={() => {this.handleShow(); this.props.moveTo(this.props.produto, 'PROMOCOES')}}>
-            <FavoriteIcon />
-          </IconButton>
-          <IconButton className="icon" onClick={() => {this.handleShow(); this.props.moveTo(this.props.produto, 'FAVORITOS')}}>
-            <BookMarkIcon /> 
-          </IconButton>
-          <IconButton className="icon" onClick={() => {this.handleShow(); this.props.moveTo(this.props.produto, 'COMPRADOS')}}>
-            <MoneyOff />
-          </IconButton>
-        </Card.Body>
-        <Modal show={this.state.show} onHide={this.handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Alterado com sucesso</Modal.Title>
-          </Modal.Header>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={this.handleClose}>
-              Ok
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </Card>
+          <Card.Img variant="top" src={this.props.tratarPath(this.props.produto.thumbnail)}  onClick={()=> this.handleShowModalCard()}/>
+          <Card.Body>
+            <Card.Title className="title" >{this.props.produto.title}</Card.Title>
+            <IconButton className="icon" onClick={() => {this.handleShow(); this.props.moveTo(this.props.produto, 'PROMOCOES')}}>
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton className="icon" onClick={() => {this.handleShow(); this.props.moveTo(this.props.produto, 'FAVORITOS')}}>
+              <BookMarkIcon /> 
+            </IconButton>
+            <IconButton className="icon" onClick={() => {this.handleShow(); this.props.moveTo(this.props.produto, 'COMPRADOS')}}>
+              <MoneyOff />
+            </IconButton>
+          </Card.Body>
+          <Modal show={this.state.show} onHide={this.handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Alterado com sucesso</Modal.Title>
+            </Modal.Header>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={this.handleClose}>
+                Ok
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </Card>
+        {this.MyModal()}
       </div>
-    )
-  }
-}
+    ) 
+  } 
 
+ }
+  
 
 const mapDispatchToProps = dispatch => {
   return {
